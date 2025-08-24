3646a146284e0a95fed9b68434fbec2c5edc91b8
Diego Nosari <diego_nos@hotmail.it>
2025-08-24 17:37:38 +0200

    Fixed animation for death, idle, running and jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,19 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var is_dead: bool = false
 
 func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if is_dead:
+		animated_sprite.rotation_degrees += -1 * (direction) * 200 * delta
+		velocity += get_gravity() * delta
+		move_and_slide()
+		return
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -14,9 +25,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	# Get the input direction: -1, 0 or 1
+	# Flip the sprite 
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+		
+		
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -24,4 +50,13 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
+func bounce():
+	velocity.y = JUMP_VELOCITY / 2
+	
+	
+func die():
+	if is_dead:
+		return
+	is_dead = true
+	bounce()
 	
